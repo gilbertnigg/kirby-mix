3acf19fcdb46ed694eb03da7b458e7809fa80c38
Gilbert Nigg <tech@gilles.ch>
2021-02-08 20:14:57 +0100

    Setup tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,38 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Helvetica', 'Roboto', 'sans-serif'],
+    },
+    screens: {
+      'xs': [
+        {'max': '639px'},
+      ],
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
+    fontSize: {
+      xs: ['0.75rem', { lineHeight: '1rem' }],
+      sm: ['0.875rem', { lineHeight: '1.25rem' }],
+      base: ['1rem', { lineHeight: '1.5rem' }],
+      lg: ['1.125rem', { lineHeight: '1.75rem' }],
+      xl: ['1.25rem', { lineHeight: '1.75rem' }],
+      '2xl': ['1.5rem', { lineHeight: '2rem' }],
+      '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
+      '4xl': ['2.25rem', { lineHeight: '2.5rem' }],
+    },
+    extend: {
+      textColor: {
+        primary: '#0A0A0A',
+        secondary: '#5a7e92',
+      },
+      colors: {
+        primary: '#5a7e92',
+        secondary: '#2c3d4a',
+      },
+    },
   },
   variants: {
     extend: {},
