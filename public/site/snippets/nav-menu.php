da6aef5fa8cfda78c2c69c837f07ee05579cfb00
Gilbert Nigg <tech@gilles.ch>
2021-04-14 20:14:03 +0200

    Add nav-menu snippet

---
+++ public/site/snippets/nav-menu.php
@@ -0,0 +1,22 @@
+<ul>
+
+	<li class="<?= ($page === $pages->get('home') ? 'is-selected' : '') ?>">
+		<?= $pages->get('home')->title()->link() ?>
+	</li>
+
+	<?php /* Level 1 */ foreach ($site->children()->listed() as $_page_l1) : ?>
+	<li class="<?= ($page === $_page_l1 || $page->parents()->has($_page_l1) ? 'is-selected' : '') ?>">
+		<?= $_page_l1->title()->link() ?>
+		<?php if ($_page_l1->children()->listed()->count()) : ?>
+		<ul>
+		<?php /* Level 2 */ foreach ($_page_l1->children()->listed() as $_page_l2) :  ?>
+			<li class="<?= ($page === $_page_l2 ? 'is-selected' : '') ?>">
+				<?= $_page_l2->title()->link() ?>
+			</li>
+		<?php endforeach ?>
+		</ul>
+		<?php endif ?>
+	</li>
+	<?php endforeach ?>
+
+</ul>
