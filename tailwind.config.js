c57e72e7210e57ce368379f5e950c98ac5877fba
Gilbert Nigg <tech@gilles.ch>
2021-04-20 14:58:42 +0200

    Change lineheight units

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,14 +34,14 @@ module.exports = {
       'xl': '1280px',
     },
     fontSize: {
-      xs: ['0.75rem', { lineHeight: '1rem' }],
-      sm: ['0.875rem', { lineHeight: '1.25rem' }],
-      base: ['1rem', { lineHeight: '1.5rem' }],
-      lg: ['1.125rem', { lineHeight: '1.75rem' }],
-      xl: ['1.25rem', { lineHeight: '1.75rem' }],
-      '2xl': ['1.5rem', { lineHeight: '2rem' }],
-      '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
-      '4xl': ['2.25rem', { lineHeight: '2.5rem' }],
+      xs: ['0.75rem', { lineHeight: '1.4' }],
+      sm: ['0.875rem', { lineHeight: '1.4' }],
+      base: ['1rem', { lineHeight: '1.4' }],
+      lg: ['1.125rem', { lineHeight: '1.4' }],
+      xl: ['1.25rem', { lineHeight: '1.3' }],
+      '2xl': ['1.5rem', { lineHeight: '1.2' }],
+      '3xl': ['1.875rem', { lineHeight: '1.2' }],
+      '4xl': ['2.25rem', { lineHeight: '1.1' }],
     },
     extend: {
       textColor: {
