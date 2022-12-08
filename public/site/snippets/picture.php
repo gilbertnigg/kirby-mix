91accb821144223d0ea6e9b7f211f87c80f585f9
Gilbert Nigg <tech@gilles.ch>
2022-12-08 11:58:54 +0100

    Update picture asset

--- public/site/snippets/picture.php
+++ public/site/snippets/picture.php
@@ -1,35 +1,37 @@
 <?php
 /**
- * @var array|int image_size
- * @var string alt
- * @var string class
+ * @var Kirby\Cms\File $img
+ * @var array|int $size
+ * @var string $alt
+ * @var string $class
  */
 
-if (is_array($image_size)) {
-	$img_width  = $image_size[0];
-	$img_height = $image_size[1];
+if (is_array($size)) {
+	$img_width  = $size[0];
+	$img_height = $size[1];
 	$crop = true;
 } else {
-	$img_width  = $image_size;
+	$img_width  = $size;
 	$img_height = 0;
 	$crop = false;
 }
-if (!isset($alt) || !$alt) $alt   = $page->title();
-if (!isset($class))        $class = '';
-
+$alt = $img->alt() ?: $page->title();
+$class = $class ?? '';
 ?>
+
 <picture>
 <source
 	type="image/webp"
-	srcset="<?= $image->srcset([
+	srcset="<?= $img->srcset([
 		'1x'=>['width'=>$img_width, 'height'=>$img_height, 'crop'=>$crop, 'format'=>'webp'],
-		'2x'=>['width'=>($img_width*2), 'height'=>($img_height*2), 'crop'=>$crop, 'format'=>'webp']
+		'2x'=>['width'=>($img_width * 2), 'height'=>($img_height * 2), 'crop'=>$crop, 'format'=>'webp']
 	]) ?>">
 <img
-	src="<?= $image->thumb(['width'=>$img_width, 'height'=>$img_height, 'crop'=>$crop])->url() ?>"
-	width="<?=	$img_width  ?: round($img_height*$image->ratio()) ?>"
-	height="<?=	$img_height ?: round($img_width*$image->ratio()) ?>"
+	src="<?= $img->thumb(['width'=>$img_width, 'height'=>$img_height, 'crop'=>$crop])->url() ?>"
+	width="<?= $img_width ?>"
+	height="<?= $img_height ?: round($img_width * $img->ratio()) ?>"
 	alt="<?= $alt ?>"
 	class="<?= $class ?>"
+	loading="lazy"
 	>
 </picture>
--- public/site/templates/default.php
+++ public/site/templates/default.php
@@ -2,20 +2,29 @@
 
 <article class="__center">
 
-	<h1><?= $page->title() ?></h1>
-	<?php if ($page->headline()->isNotEmpty()) : ?>
-		<h3><?= $page->headline()->kti() ?></h3>
-	<?php endif ?>
-	<?= $page->text() ?>
+<h1><?= $page->title() ?></h1>
+<?php if ($page->headline()->isNotEmpty()) : ?>
+	<h3><?= $page->headline()->kti() ?></h3>
+<?php endif ?>
 
-	<?php foreach ($page->images()->template('image')->sortBy('sort', 'asc', 'filename', 'asc') as $img) : ?>
+<?php if ($img = $page->images()->template('cover')->first()) : ?>
+<p>
 	<figure>
-	    <img src="<?= $img->resize(1200)->url() ?>" alt="<?= $img->caption()->isNotEmpty() ? $img->caption() : $page->title() ?>">
-	    <?php if ($img->caption()->isNotEmpty()) : ?>
-	    <figcaption><?= $img->caption() ?></figcaption>
-	    <?php endif ?>
+		<img src="<?= $img->resize(1200)->url() ?>" alt="<?= $img->alt()->isNotEmpty() ? $img->alt() : $page->title() ?>">
+		<?php if ($img->alt()->isNotEmpty()) : ?>
+		<figcaption><?= $img->alt() ?></figcaption>
+		<?php endif ?>
 	</figure>
-	<?php endforeach ?>
+</p>
+<?php endif ?>
+
+<?= $page->text() ?>
+
+<?php foreach ($page->images()->template('image')->sortBy('sort', 'asc', 'filename', 'asc') as $img) : ?>
+<p>
+	<?php snippet('picture', ['img'=>$img, 'size'=>1200]) ?>
+</p>
+<?php endforeach ?>
 
 </article>
 
