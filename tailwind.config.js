85c0daf87f5b2ee60c0c161e641ee0871ff40649
Gilbert Nigg <tech@gilles.ch>
2023-04-24 15:21:04 +0200

    Update tailwind layers

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,38 +16,30 @@ module.exports = {
   safelist: ['is-selected', 'nav-open'],
   darkMode: 'media',
   theme: {
-    fontFamily: {
-      sans: ['Helvetica', 'Roboto', 'sans-serif'],
-    },
     screens: {
       'sm': '640px',
       'md': '768px',
       'lg': '1024px',
       'xl': '1280px',
     },
+    fontFamily: {
+      'sans': ['Helvetica', 'sans-serif'],
+    },
     fontSize: {
-      xs: ['0.75rem', { lineHeight: '1.4' }],
-      sm: ['0.875rem', { lineHeight: '1.4' }],
-      base: ['1rem', { lineHeight: '1.4' }],
-      lg: ['1.125rem', { lineHeight: '1.4' }],
-      xl: ['1.25rem', { lineHeight: '1.3' }],
-      '2xl': ['1.5rem', { lineHeight: '1.2' }],
-      '3xl': ['1.875rem', { lineHeight: '1.2' }],
-      '4xl': ['2.25rem', { lineHeight: '1.1' }],
+      'base': ['1.0rem', 1.4],
+      'lg': ['1.5rem', 1.4],
+      'xl': ['2rem', 1.4],
     },
     extend: {
-      textColor: {
-        primary: '#0A0A0A',
-        secondary: '#5a7e92',
-      },
       colors: {
-        primary: '#5a7e92',
-        secondary: '#2c3d4a',
+        'primary': '#5b7f95',
+        'secondary': '#2c3d4a',
+      },
+      textColor: {
+        'body': '#0A0A0A',
       },
     },
   },
-  variants: {
-    extend: {},
-  },
   plugins: [],
-};
+}
+
