64423298eebd83724f5ab2c9047c5d6730fe4d8d
Gilbert Nigg <tech@gilles.ch>
2023-09-25 18:22:56 +0200

    Update navicon

--- public/site/snippets/header.php
+++ public/site/snippets/header.php
@@ -38,8 +38,12 @@ document.documentElement.classList.add('is-js');
 
 <header>
 	<button id="navicon" aria-label="Menu open/close">
-		<span class="is-open">&plus;</span>
-		<span class="is-closed">&times;</span>
+		<span class="is-open">
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" /></svg>
+		</span>
+		<span class="is-closed">
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" /></svg>
+		</span>
 	</button>
 </header>
 
