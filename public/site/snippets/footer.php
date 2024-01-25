6381129ffe4e5bc7da9d6daae48d7829ec3cda1c
Gilbert Nigg <tech@gilles.ch>
2024-01-25 09:30:51 +0100

    Simplify if statement

--- public/site/snippets/footer.php
+++ public/site/snippets/footer.php
@@ -3,16 +3,16 @@
 <footer class="py-8 mt-16 bg-primary">
 	<div class="__center text-white [&_a]:text-white md:flex md:justify-items-start">
 		<div class="md:flex-1 md:flex md:space-x-4">
-			<?php if ($site->footer_address()) : ?>
+			<?php if ($site->footer_address()->isNotEmpty()) : ?>
 			<address class="not-italic">
 				<?= $site->footer_address() ?>
 			</address>
 			<?php endif ?>
-			<?php if ($site->footer_links()->isNotEmpty()) : ?>
+			<?php if ($site->footer_links()->toPages()->count()) : ?>
 			<nav id="nav-footer">
 				<ul>
 				<?php foreach ($site->footer_links()->toPages() as $link) : ?>
-					<li class="<?= ($link === $page ? 'is-selected' : '') ?>">
+					<li class="<?= e($link === $page, 'is-selected') ?>">
 						<?= $link->title()->link() ?>
 					</li>
 				<?php endforeach ?>
--- public/site/snippets/nav-main.php
+++ public/site/snippets/nav-main.php
@@ -1,12 +1,12 @@
 <ul class="[&_li.is-selected>a]:text-secondary [&_li>a:hover]:text-secondary">
 
 	<?php /* Level 1 */ foreach ($site->children()->listed() as $main_page) : ?>
-	<li class="<?= ($page === $main_page || $page->parents()->has($main_page) ? 'is-selected' : '') ?>">
+	<li class="<?= e($page === $main_page || $page->parents()->has($main_page), 'is-selected') ?>">
 		<?= $main_page->title()->link() ?>
 		<?php if ($main_page->children()->listed()->count()) : ?>
 		<ul>
 		<?php /* Level 2 */ foreach ($main_page->children()->listed() as $sub_page) :  ?>
-			<li class="<?= ($page === $sub_page ? 'is-selected' : '') ?>">
+			<li class="<?= e($page === $sub_page, 'is-selected') ?>">
 				<?= $sub_page->title()->link() ?>
 			</li>
 		<?php endforeach ?>
