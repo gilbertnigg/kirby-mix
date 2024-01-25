1a25fce0b93ba34953db3f996f1ec8b129ea8fca
Gilbert Nigg <tech@gilles.ch>
2024-01-25 20:37:25 +0100

    Update nav

--- public/site/snippets/header.php
+++ public/site/snippets/header.php
@@ -38,15 +38,15 @@ document.documentElement.classList.replace('no-js', 'is-js');
 <header>
 	<button id="navicon" aria-label="Menu open/close">
 		<span class="is-open">
-			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" /></svg>
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8"><path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" /></svg>
 		</span>
 		<span class="is-closed">
-			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" /></svg>
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" /></svg>
 		</span>
 	</button>
 </header>
 
-<nav class="is-responsive is-dropdown">
+<nav id="nav-main" class="is-responsive is-dropdown">
 	<?php snippet('nav-main') ?>
 </nav>
 
