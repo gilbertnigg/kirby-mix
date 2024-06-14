3b92c5a00c9a59e0cfccc8aa7fe08f9d6d0ada4a
Gilbert Nigg <tech@gilles.ch>
2024-06-14 09:37:59 +0200

    Update project-controller

--- public/site/controllers/project.php
+++ public/site/controllers/project.php
@@ -1,7 +1,14 @@
 <?php
 return function ($page) {
 	$seoImage = $page->images()->template('cover')->first();
+	$seoImage = $page->images()->template('cover')->first();
+	$projects = $page->siblings();
+	$prev = $page->prevListed($projects);
+	$next = $page->nextListed($projects);
 	return [
-		'seoImage' => $seoImage
+		'seoImage' => $seoImage,
+		'projects' => $projects,
+		'prev' => $prev,
+		'next' => $next
 	];
 };
