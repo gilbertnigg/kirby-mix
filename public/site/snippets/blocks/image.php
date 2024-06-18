8ab6e8c167878d6eddb00c0eea2d2668c30e6d39
Gilbert Nigg <tech@gilles.ch>
2024-06-18 13:09:39 +0200

    Update video-/imageblock

--- public/site/snippets/blocks/image.php
+++ public/site/snippets/blocks/image.php
@@ -1,7 +1,8 @@
 <?php /** @var \Kirby\Cms\Block $block */ ?>
 
-<?php if ($image = $block->image()->toFile()) : ?>
-<figure>
+<?php if ($image = $block->image()->toFile()) :
+$img_size = $block->image_size()->value(); ?>
+<figure class="block my-12 <?= ($img_size==='sm' ? 'w-1/3' : ($img_size==='md' ? 'w-2/3' : 'w-full')) ?>">
 	<picture>
 		<img
 		srcset="<?= $image->srcset([
