927cf75f847c12434c812a07a66ade791714110f
Gilbert Nigg <30411415+gilbertnigg@users.noreply.github.com>
2024-09-17 16:15:16 +0200

    Update config

--- public/site/config/config.php
+++ public/site/config/config.php
@@ -4,11 +4,11 @@ return [
 	'debug' => ( // enable debug mode for local testing
 		stripos($_SERVER['HTTP_HOST'], 'test') !== false  ? true : false
 	),
-	'auth' => [
-		'methods' => ['password', 'password-reset']
-	],
 	'thumbs' => [ // choose im for local testing
 		'driver' => stripos($_SERVER['HTTP_HOST'], 'test') !== false  ? 'gd' : 'im'
+	],
+	'auth' => [
+		'methods' => ['password', 'password-reset']
 	],
 	// 'languages' => true,
 	'panel' => [
