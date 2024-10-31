356759aa995474fb55574e8c757f507859dc2661
Gilbert Nigg <30411415+gilbertnigg@users.noreply.github.com>
2024-10-31 09:34:56 +0100

    Remove has- pattern until its needed

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
     './public/site/templates/**/*.php',
     './src/scripts/**/*.js',
   ],
-  safelist: [{ pattern: /is-(.+)/ }, { pattern: /has-(.+)/ }],
+  safelist: [{ pattern: /is-(.+)/ }],
   darkMode: 'media',
   theme: {
     screens: {
