a9321b001d76d1ea639e9242b846766f52908ea5
Gilbert Nigg <30411415+gilbertnigg@users.noreply.github.com>
2025-01-15 08:55:38 +0100

    Add esc key

--- src/scripts/_navicon.js
+++ src/scripts/_navicon.js
@@ -2,3 +2,9 @@ document.getElementById('navicon').addEventListener('click', (e) => {
   e.preventDefault();
   document.documentElement.classList.toggle('is-nav-open');
 });
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') {
+    document.documentElement.classList.remove('is-nav-open');
+  }
+});
