73bdc42c9f87a2a809501764368fe60262852373
Gilbert Nigg <tech@gilles.ch>
2025-07-14 18:44:56 +0200

    No Home on nav

--- site/snippets/nav-main.php
+++ site/snippets/nav-main.php
@@ -1,6 +1,6 @@
 <ul class="[&_li.is-selected>a]:text-secondary [&_li>a:hover]:text-secondary">
 
-	<?php /* Level 1 */ foreach ($site->children()->listed() as $main_page) : ?>
+	<?php /* Level 1 */ foreach ($site->children()->notTemplate('home')->listed() as $main_page) : ?>
 	<li class="<?= e($page === $main_page || $page->parents()->has($main_page), 'is-selected') ?>">
 		<?= $main_page->title()->link() ?>
 		<?php if ($main_page->children()->listed()->count()) : ?>
