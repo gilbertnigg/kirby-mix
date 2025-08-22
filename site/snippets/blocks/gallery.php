d2575c50f670649c3c28867a5b0b5ab9de9e185d
Gilbert Nigg <tech@gilles.ch>
2025-08-22 09:59:29 +0200

    fix gallery

--- site/snippets/blocks/gallery.php
+++ site/snippets/blocks/gallery.php
@@ -2,9 +2,12 @@
 /** @var \Kirby\Cms\Block $block */
 ?>
 <div class="grid grid-cols-2 gap-2 items-center md:grid-cols-3 lg:grid-cols-4 lg:gap-4">
-<?php foreach ($block->images()->toFiles() as $image): ?>
-	<a href="<?= $image->url() ?>" class="no-underline" target="_blank">
-		<?= $image->thumb(['width'=>800, 'height'=>800]) ?>
-	</a>
-	<?php endforeach ?>
-</figure>
+    <?php foreach ($block->gallery()->toFiles() as $image) :
+	$thumb = $image->thumb(['width'=>800, 'height'=>800]);
+	?>
+    <a href="<?= $image->url() ?>" class="inline-block no-underline" target="_blank">
+        <img src="<?= $thumb->url() ?>" width="<?= $thumb->width() ?>" height="<?= $thumb->height() ?>"
+            alt="<?= $image->alt() ?>" loading="lazy">
+    </a>
+    <?php endforeach ?>
+    </figure>
