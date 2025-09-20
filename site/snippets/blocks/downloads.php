a23a9f2a269e4aec1ddd0bf3b81fb3a0644c87b2
Gilbert Nigg <tech@gilles.ch>
2025-09-20 13:50:01 +0200

    Update blocks

--- site/snippets/blocks/downloads.php
+++ site/snippets/blocks/downloads.php
@@ -1,15 +1,7 @@
 <?php
 /** @var \Kirby\Cms\Block $block */
-?>
 
-<?php if ($files->count()) : ?>
-<ul>
-	<?php foreach ($block->files()->toFiles() as $file): ?>
-	<li>
-		<a href="<?= $file->url() ?>" title="<?= $file->filename().' - '.$file->niceSize() ?>" target="_blank">
-			<?= $file->description()->or($file->name()) ?>
-		</a>
-	</li>
-	<?php endforeach ?>
-</ul>
-<?php endif ?>
+$files = $block->files()->toFiles();
+snippet('components/downloads', ['files' => $files]);
+
+?>
---
+++ site/snippets/components/downloads.php
@@ -0,0 +1,15 @@
+<?php
+/** @var \Kirby\Cms\Files $files */
+?>
+
+<?php if ($files->count()) : ?>
+<ul>
+	<?php foreach ($files as $file): ?>
+	<li>
+		<a href="<?= $file->url() ?>" title="<?= $file->filename().' - '.$file->niceSize() ?>" target="_blank" rel="noopener noreferrer">
+			<?= $file->description()->or($file->name()) ?>
+		</a>
+	</li>
+	<?php endforeach ?>
+</ul>
+<?php endif ?>
