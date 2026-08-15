374c713d36da573c3a44daad16aa513b1cfde6f0
Gilbert Nigg <tech@gilles.ch>
2026-08-15 10:13:59 +0200

    Update nav @var

--- site/snippets/nav-main.php
+++ site/snippets/nav-main.php
@@ -1,12 +1,18 @@
+<?php
+/**
+ * @var \Kirby\Cms\Page $page
+ * @var \Kirby\Cms\Site $site
+ */
+?>
 <ul class="[&_li.is-selected>a]:text-secondary [&_li>a:hover]:text-secondary">
 
 	<?php /* Level 1 */ foreach ($site->children()->notTemplate('home')->listed() as $main_page) : ?>
-	<li class="<?= e($page === $main_page || $page->parents()->has($main_page), 'is-selected') ?>">
+	<li class="<?= $page === $main_page || $page->parents()->has($main_page) ? 'is-selected' : '' ?>">
 		<?= $main_page->title()->link() ?>
 		<?php if ($main_page->children()->listed()->count()) : ?>
 		<ul>
 		<?php /* Level 2 */ foreach ($main_page->children()->listed() as $sub_page) :  ?>
-			<li class="<?= e($page === $sub_page, 'is-selected') ?>">
+			<li class="<?= $page === $sub_page ? 'is-selected' : '' ?>">
 				<?= $sub_page->title()->link() ?>
 			</li>
 		<?php endforeach ?>
