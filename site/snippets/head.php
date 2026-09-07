1a13de383aa1542d7363e259b96e45e16221927d
Gilbert Nigg <tech@gilles.ch>
2026-09-07 10:25:24 +0200

    Add vars

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -1,3 +1,9 @@
+<?php
+/**
+ * @var Kirby\Cms\Page $page
+ * @var int $cache
+ */
+?>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width,initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
